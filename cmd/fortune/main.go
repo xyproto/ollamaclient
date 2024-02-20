ee36e1e0fb6532270e4af411b88059b0a6ebc5ba
Alexander F. Rødseth <rodseth@gmail.com>
2024-02-20 22:08:46 +0100

    Add a new example program

---
+++ cmd/fortune/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/xyproto/ollamaclient"
+)
+
+const (
+	model  = "tinydolphin"
+	prompt = "Write a silly saying, quote or joke like it could have been the output of the fortune command on Linux."
+)
+
+func main() {
+	oc := ollamaclient.NewWithModel(model)
+
+	err := oc.PullIfNeeded(true)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to pull model: %v", err)
+		os.Exit(1)
+	}
+
+	if !oc.Has("tinyllama") {
+		fmt.Fprintln(os.Stderr, "Expected to have 'tinyllama' model downloaded, but it's not present")
+		os.Exit(1)
+	}
+
+	oc.SetRandomOutput()
+
+	generatedOutput := oc.MustOutput(prompt)
+	if generatedOutput == "" {
+		log.Println("fail")
+	}
+
+	fmt.Println(ollamaclient.Massage(generatedOutput))
+}
