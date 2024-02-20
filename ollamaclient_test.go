bfa0f6fcdee8adbf2b422900bc95e01acd9b6266
Alexander F. Rødseth <rodseth@gmail.com>
2024-02-20 13:50:29 +0100

    Update the test

--- ollamaclient_test.go
+++ ollamaclient_test.go
@@ -1,13 +1,15 @@
 package ollamaclient
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestPullTinyLlamaIntegration(t *testing.T) {
 	oc := NewWithModel("tinyllama")
 
-	_, err := oc.Pull(true)
+	err := oc.PullIfNeeded(true)
 	if err != nil {
 		t.Fatalf("Failed to pull model: %v", err)
 	}
@@ -17,4 +19,49 @@ func TestPullTinyLlamaIntegration(t *testing.T) {
 	} else {
 		t.Log("Pull operation completed successfully, 'tinyllama' model is present")
 	}
+
+	generatedOutput := oc.MustOutput("Generate an imperative sentence. Keep it brief. Only output the sentence itself. Skip explanations, introductions or preamble.")
+	if generatedOutput == "" {
+		t.Fail()
+	}
+	s := generatedOutput
+
+	// Keep the part after ":", if applicable
+	if strings.Contains(s, ":") {
+		parts := strings.SplitN(s, ":", 2)
+		s = strings.TrimSpace(parts[1])
+	}
+	// Keep the part within double quotes, if applicable
+	if strings.Count(s, "\"") == 2 {
+		parts := strings.SplitN(s, "\"", 3)
+		s = strings.TrimSpace(parts[1])
+	}
+	// Keep the part after ":", if applicable
+	if strings.Contains(s, ":") {
+		parts := strings.SplitN(s, ":", 2)
+		s = strings.TrimSpace(parts[1])
+	}
+	// Remove stray quotes
+	s = strings.TrimPrefix(s, "\"")
+	s = strings.TrimPrefix(s, "'")
+	s = strings.TrimSuffix(s, "\"")
+	s = strings.TrimSuffix(s, "'")
+
+	// Keep the last line
+	if strings.Count(s, "\n") > 1 {
+		lines := strings.Split(s, "\n")
+		s = lines[len(lines)-1]
+	}
+
+	// Keep the part before the exclamation mark, if applicable
+	if strings.Contains(s, "!") {
+		parts := strings.SplitN(s, "!", 2)
+		s = strings.TrimSpace(parts[0]) + "!"
+	}
+
+	if s == "" {
+		s = generatedOutput
+	}
+
+	fmt.Println(s)
 }
