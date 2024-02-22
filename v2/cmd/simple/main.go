2653cb9cb23dc8344639b06344bcce1ed624e9c5
Alexander F. Rødseth <rodseth@gmail.com>
2024-02-22 14:37:17 +0100

    Make the simple example even simpler

--- v2/cmd/simple/main.go
+++ v2/cmd/simple/main.go
@@ -8,19 +8,15 @@ import (
 
 func main() {
 	oc := ollamaclient.New()
-	oc.ModelName = "tinyllama"
-
 	if err := oc.PullIfNeeded(true); err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
 	prompt := "Write a haiku about the color of cows."
 	output, err := oc.GetOutput(prompt)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-
 	fmt.Println(output)
 }
