6aa6f86132a8e48fe92591a6d6fb7d18a4c1582d
Alexander F. Rødseth <rodseth@gmail.com>
2024-05-01 16:54:43 +0200

    Add an example for a simple way to describe images

---
+++ v2/cmd/simpledescribe/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/xyproto/ollamaclient/v2"
+)
+
+func main() {
+	oc := ollamaclient.New()
+	oc.ModelName = "llava"
+	oc.SetReproducible()
+	if err := oc.PullIfNeeded(true); err != nil {
+		log.Fatalln(err)
+	}
+	imageFilenames := []string{"carrot1.png", "carrot2.png"}
+	const desiredWordCount = 7
+	description, err := oc.DescribeImages(imageFilenames, desiredWordCount)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(description)
+}
