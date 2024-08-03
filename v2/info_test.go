b49b63f609acf516c30e893f2e2b588a64776821
Jonathan Hecl <jonathanhecl@gmail.com>
2024-08-03 01:16:08 -0300

    GetShowInfo

---
+++ v2/info_test.go
@@ -0,0 +1,26 @@
+package ollamaclient
+
+import "testing"
+
+func TestGetShowInfo(t *testing.T) {
+	oc := New("llama3.1")
+	oc.Verbose = true
+
+	err := oc.PullIfNeeded(true)
+	if err != nil {
+		t.Fatalf("Failed to pull model: %v", err)
+	}
+
+	if found, err := oc.Has("llama3.1"); err != nil || !found {
+		t.Error("Expected to have 'llama3.1' model downloaded, but it's not present")
+	}
+
+	res, err := oc.GetShowInfo()
+	if err != nil {
+		t.Fatalf("Failed to get show info: %v", err)
+	}
+
+	if res.ModelInfo.LlamaContextLength != 131072 {
+		t.Errorf("Expected context length to be 131072, but got %d", res.ModelInfo.LlamaContextLength)
+	}
+}
